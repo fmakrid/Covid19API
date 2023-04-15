d721a924c1b571dc4c2d8a5dad13d276cc0d6d05
Filippos Makridis <philippos.makridis@three-pi.com>
2023-04-16 01:20:59 +0300

    Fixed sql connection & query to work on PlanetScale Added error handler query Added .env functionality for more secure connection with only read permisions

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+
+
+require("dotenv").config();
 
 // this code is used to access the body properties of DOM
 // to extract GET/POST requests
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const hostname = "localhost";
 const port = 8080;
 
-
 const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+
+
 
 // This runs an sql query, cleans the results
 // and outputs them in the specified file as needed
-function sqlQuery(data, callback) {
-    const sql = "SELECT iso2, cases FROM covid19 WHERE dates='" + data + "';";
-    connection.query(sql, function(err, results, fields) {
-      // if (err) {
-      //   throw err;
-      // }
-      let cleanResults = results.map();
-      for (let i of cleanResults) {
-        let test = i.iso2;
-        let test2 = i.cases;
-        gdpData[test] = test2;
+function sqlQuery(date, callback) {
+    const query = `SELECT * FROM covid19 WHERE dates = '${date}'`;
+    connection.query(query, function (error, results, fields) {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, results);
       }
-      stuffIWant = gdpData;
-      return callback(gdpData);
     });
   }
 
 
 
-app.get('/api/data/', (req, res) => {
-    const date = req.query.date;
-    res.send(`Date you selected is ${date}`);
-  });
+app.get("/api/data/", (req, res) => {
+	const date = req.query.date;
+    sqlQuery(date, function (error, results) {
+        if (error) {
+          console.error(error);
+        } else {
+          res.send(results);
+        }
+      });
+});
 
-  
-  app.listen(port, () => {
-    console.log('Server running on port 3000');
-  });
-  
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
