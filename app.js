cbc6d58a471f3722f2a8cfff94b1b4a286f730b2
Filippos Makridis <philippos.makridis@three-pi.com>
2023-04-15 21:27:20 +0300

    Added mysql database functionality Changed port from 3000 to 8080

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2')
+
+// this code is used to access the body properties of DOM
+// to extract GET/POST requests
+app.use(express.urlencoded({extended: false}));
+
+const hostname = "localhost";
+const port = 8080;
+
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+// This runs an sql query, cleans the results
+// and outputs them in the specified file as needed
+function sqlQuery(data, callback) {
+    const sql = "SELECT iso2, cases FROM covid19 WHERE dates='" + data + "';";
+    connection.query(sql, function(err, results, fields) {
+      // if (err) {
+      //   throw err;
+      // }
+      let cleanResults = results.map();
+      for (let i of cleanResults) {
+        let test = i.iso2;
+        let test2 = i.cases;
+        gdpData[test] = test2;
+      }
+      stuffIWant = gdpData;
+      return callback(gdpData);
+    });
+  }
+
 
 
 app.get('/api/data/', (req, res) => {
@@ -8,7 +39,7 @@ app.get('/api/data/', (req, res) => {
   });
 
   
-  app.listen(3000, () => {
+  app.listen(port, () => {
     console.log('Server running on port 3000');
   });
   
