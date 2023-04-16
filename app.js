a34a72032da8644fe7ac3c5bb5ba5ebed17c18ff
Filippos Makridis <philippos.makridis@three-pi.com>
2023-04-16 18:49:04 +0300

    BUGFIX: Fixed CORS issue BUGFIX: Fixed issue of map data not parsing correctly BUGFIX: Fixed data query returning as datetime

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
 const express = require("express");
 const app = express();
-const mysql = require('mysql2');
-
+const mysql = require("mysql2");
+const cors = require("cors");
 
 require("dotenv").config();
 
 // this code is used to access the body properties of DOM
 // to extract GET/POST requests
-app.use(express.urlencoded({ extended: false }));
 
-const hostname = "207.154.212.148";
+app.use(cors());
+
 const port = 8080;
 
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-
-
-
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
+	ssl: { rejectUnauthorized: true },
+	dateStrings: true,
+});
 
 // This runs an sql query, cleans the results
 // and outputs them in the specified file as needed
 function sqlQuery(date, callback) {
-    const query = `SELECT * FROM covid19 WHERE dates = '${date}'`;
-    connection.query(query, function (error, results, fields) {
-      if (error) {
-        callback(error, null);
-      } else {
-        callback(null, results);
-      }
-    });
-  }
-
-
+	const query = `SELECT iso2,cases FROM covid19 WHERE dates=${date}`;
+	connection.query(query, function (error, results) {
+		if (error) {
+			callback(error, null);
+		} else {
+			callback(null, results);
+		}
+	});
+}
 
 app.get("/api/data/", (req, res) => {
 	const date = req.query.date;
-    sqlQuery(date, function (error, results) {
-        if (error) {
-          console.error(error);
-        } else {
-          res.send(results);
-        }
-      });
+	sqlQuery(date, function (error, results) {
+		if (error) {
+			console.error(error);
+		} else {
+			res.send(results);
+		}
+	});
 });
 
 app.listen(port, () => {
